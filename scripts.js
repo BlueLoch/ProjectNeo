b6cd35b97683276ec47a887b751bfeb0afddd194
akilgour350 <akilgour350@gmail.com>
2024-02-08 20:44:42 +0000

    nessie goes nuts

--- scripts.js
+++ scripts.js
@@ -24,13 +24,13 @@ function setTheme() {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
             sessionStorage.setItem('viewMode', 'dark');
             document.getElementById('docBody').classList.add('darkmode');
-            document.getElementById('sidelogo').src = "/resources/img/logos/Text-white.png";
-            document.getElementById('sidelogo-icon-img').src = "/resources/img/logos/Nessy-white-filled.png";
+            document.getElementById('sidelogo').src = "/resources/img/logos/Text-black.png";
+            document.getElementById('sidelogo-icon-img').src = "/resources/img/logos/Nessy-black-filled.png";
         } else {
             sessionStorage.setItem('viewMode', 'light');
             document.getElementById('docBody').classList.remove('darkmode');
-            document.getElementById('sidelogo').src = "/resources/img/logos/Text-black.png";
-            document.getElementById('sidelogo-icon-img').src = "/resources/img/logos/Nessy-black-filled.png";
+            document.getElementById('sidelogo').src = "/resources/img/logos/Text-white.png";
+            document.getElementById('sidelogo-icon-img').src = "/resources/img/logos/Nessy-white-filled.png";
         }
     }
 
@@ -125,3 +125,54 @@ function checkCookies() {
         sessionStorage.setItem('seenCookies', 'yes');
     }
 }
+
+
+
+// Fun stuff for Nessie. I will protect this creature with my life
+const pause = ms => new Promise(res => setTimeout(res, ms));
+
+function checkAbilities() {
+    if (document.getElementById('docBody').clientWidth > 800) {
+        document.getElementById('nessie').addEventListener('dblclick', goForSwim);
+    }
+}
+
+async function doAFlip() {
+    let x = document.getElementById('nessie');
+    x.animate([
+        { transform: "rotate(0deg)" },
+        { transform: "rotate(100deg)" },
+        { transform: "rotate(300deg)" },
+        { transform: "rotate(360deg)" },
+    ],
+        {
+            duration: 1000,
+            iterations: 1,
+        });
+}
+
+async function goForSwim() {
+    let x = document.getElementById('nessie');
+    let dist = document.getElementById('nessie-cage').clientWidth - 450;
+
+    if (dist > 0) {
+        x.animate([
+            { transform: "translateX(0) rotateY(0deg)" },
+            { transform: "translateX(-" + dist + "px) rotateY(0deg)" },
+            { transform: "translateX(-" + dist + "px) rotateY(180deg)" },
+            { transform: "translateX(0) rotateY(180deg)" },
+            { transform: "translateX(0) rotateY(0deg)" },
+        ],
+            {
+                duration: 2000,
+                iterations: 1,
+            });
+    }
+}
+
+/* // this function doesnt exist. shhhhhh
+function albaAppears() {
+    console.log("called");
+    let x = document.getElementById('nessie');
+    x.src = "/resources/img/logos/alba.png";
+}*/
